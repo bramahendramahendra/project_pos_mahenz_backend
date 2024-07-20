846c8f44f745f286bdd62ea95d1393de7c55e8b7
Brama  Hendra Mahendra <jdoe@example.com>
2024-07-20 15:20:35 +0700

    category handler

---
+++ category-api/handler/handler.go
@@ -0,0 +1,100 @@
+package handler
+
+import (
+	"net/http"
+	"project/category-api/model"
+	"project/category-api/service"
+	"strconv"
+
+	"github.com/gin-gonic/gin"
+)
+
+func GetAllCategories(c *gin.Context) {
+	categories, err := service.GetAllCategories()
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		return
+	}
+
+	c.JSON(http.StatusOK, categories)
+}
+
+func GetCategoryByID(c *gin.Context) {
+	id, err := strconv.Atoi(c.Param("id"))
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "Invalid ID"})
+		return
+	}
+
+	category, err := service.GetCategoryByID(uint64(id))
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		return
+	}
+
+	if category == nil {
+		c.JSON(http.StatusNotFound, gin.H{"error": "Category not found"})
+		return
+	}
+
+	c.JSON(http.StatusOK, category)
+}
+
+func CreateCategory(c *gin.Context) {
+	var category model.Category
+	if err := c.ShouldBindJSON(&category); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+
+	if err := service.CreateCategory(&category); err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		return
+	}
+
+	c.JSON(http.StatusCreated, category)
+}
+
+func UpdateCategory(c *gin.Context) {
+	var category model.Category
+	if err := c.ShouldBindJSON(&category); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+
+	if err := service.UpdateCategory(&category); err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		return
+	}
+
+	c.JSON(http.StatusOK, category)
+}
+
+func DeleteCategory(c *gin.Context) {
+	id, err := strconv.Atoi(c.Param("id"))
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "Invalid ID"})
+		return
+	}
+
+	if err := service.DeleteCategory(uint64(id)); err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		return
+	}
+
+	c.JSON(http.StatusNoContent, nil)
+}
+
+func DeletePermanentlyCategory(c *gin.Context) {
+	id, err := strconv.Atoi(c.Param("id"))
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "Invalid ID"})
+		return
+	}
+
+	if err := service.DeletePermanentlyCategory(uint64(id)); err != nil {
+		return
+	}
+
+	c.JSON(http.StatusNoContent, nil)
+}
